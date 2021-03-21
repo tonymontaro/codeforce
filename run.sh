dad360fa4ecade8c78226de0a2342ba68fbc668d
Anthony Ngene <ngeneanthony@gmail.com>
2021-03-21 06:00:03 +0100

    update questions by cronjob.

--- run.sh
+++ run.sh
@@ -1,18 +1,7 @@
-
-
-# node a.js
 echo "==================="
 date
 /usr/local/bin/node js/problemset.js
  git add .
  git commit -m "update questions by cronjob."
  git push
-# cd /Users/tonymontaro/dev/codeforce
-# cd /Users/tonymontaro/dev/codeforce && echo "========" && date && node js/problemset.js && git add . &&  
-# git commit -m "update questions by cronjob." && git push
-# /usr/local/bin/
-
-# git add .
-
-# git commit -m "update questions by cronjob."
-
+ 
